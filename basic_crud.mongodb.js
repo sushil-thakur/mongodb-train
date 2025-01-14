62159f45649541f8ad140a1c082f040d061b90d7
sushil thakur <sushilt2059@gmail.com>
2025-01-14 19:08:33 +0545

    push

--- basic_crud.mongodb.js
+++ basic_crud.mongodb.js
@@ -35,4 +35,87 @@ use("big-mart");
 
 // },
 // ]);
+
+
+//?read
+//?findOne , find
+//? find products whose brand is "Amul"
+// db.products.insertOne({
+//     brand:"Amul",
+//     price:100,
+//     name: "liquid butter",
+//     quantity:2
+    
+
+// });
+// db.products.find({brand:"canta"});
+
+//?findOne=> return first document which matches the condition
+// db.products.findOne({brand:"Amul"});
+
+//?deleteOne
+//?deleteMany
+//?deleteOne => delete first document which matches the condition
+
+//?delete product whoch name is "Rice"
+
+//?deletemany
+//?delete all products whose brand is "Amul"
+
+// db.products.deleteOne({name:"Rice"});
+// db.products.deleteMany({brand:"Amul"});
+
+
+// db.products.insertOne({
+//     name:"sunscreen",
+//     price:50,
+//     brand:"plum",
+//     quantity:"200ml",
+// });
+
+//? to delete all product in tabel
+//? leave the condition empty
+// db.products.deleteMany({});
+
+//?update
+//?updateOne =>update single document
+//?updateMany => update multiple documents
+
+
+//?updateOne
+//? update quantoity of product whose name is "Bread" to 10
+// db.products.updateOne(
+//     {name:"Bread"},
+//     {
+//         $set:{
+//             quantity:10,
+//         }
+// }
+// );
+
+//? update  bread brand from "Britannia" to "helmer"
+// db.products.updateOne(
+//     {name:"Bread"},
+//     {
+//         $set:{
+//             brand:"helmer",
+//         }
+// }
+// );
+
+//?update quantity of butter to 20
+// db.products.updateOne({
+//     name:"Butter"
+// },{
+//     $set:{
+//         quantity:20,
+//     }
+// })
+
+//? updateMany
+//?set price of all products to 1000
+db.products.updateMany({},{
+    $set:{price:1000,
+}
+})
 db.products.find();
---
+++ practice.mongodb.js
@@ -0,0 +1,38 @@
+use("patan-hospital");
+// db.patients.insertMany([{
+//     name: "Ramesh",
+//     age: 23,
+//     gender:"male",
+//     isMarried:false,
+//     phoneNum:"9845671234",
+
+// },
+// {
+//     name:"Rohil karki",
+//     age: 34,
+//     gender:"male",
+//     isMarried:true,
+//     phoneNum:"9845671234",
+// },
+// ])
+
+// db.patients.insertOne({
+//     name:"suhana",
+//     age:23,
+//     isMarried:false,
+//     gender:"female",
+//     phoneNum:"9845671234",
+// });
+
+//?read
+// db.patients.find({name:"Ramesh"});
+//?updateOne
+// db.patients.updateOne({name:"Ramesh"},{$set:{age:24}});
+//?delete
+// db.patients.deleteOne({name:"Ramesh"})
+//?update many
+// db.patients.updateMany({},{$set:{phoneNum:"9845671234"}});
+//?delete many
+// db.patients.deleteMany({},{$set:{phoneNum:"9845671234"}});
+db.patients.find();
+
