c7607330de8ed8cf6aee744aad5a1ce49aeb5eb6
sushil thakur <sushilt2059@gmail.com>
2025-01-17 10:23:38 +0545

    push

---
+++ aggerate.mongodb.js
@@ -0,0 +1,13 @@
+use("student-management");
+// db.movies.find();
+//aggregate=> read operaton
+//does not change content of db
+
+//?pipeline stages
+// $match
+// $project
+//$sort
+//$limit
+//$skip
+//$lookup
+//$unwind
